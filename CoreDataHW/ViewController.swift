5424b30513ea2a1ca51455fe3566e6d84de2c816
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-08-18 19:43:31 +0300

    Initial Commit

---
+++ CoreDataHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataHW
+//
+//  Created by Виталий Таран on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
